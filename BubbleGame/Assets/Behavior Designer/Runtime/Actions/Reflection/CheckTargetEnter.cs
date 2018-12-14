edc4262607024b25308c9bebf107accda24cbe7b
Unknown <irrsfunmaker@gmail.com>
2018-12-14 18:52:18 +0900

    ハリネズミの攻撃部を仮実装その二

---
+++ BubbleGame/Assets/Behavior Designer/Runtime/Actions/Reflection/CheckTargetEnter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    public class CheckTargetEnter : Conditional
+    {
+        public SharedGameObject Target;
+
+        private bool hasGetTarget = false;
+        public override TaskStatus OnUpdate()
+        {
+            if (hasGetTarget)
+                return TaskStatus.Success;
+            else
+            {
+                return TaskStatus.Failure;
+            }
+        }
+
+        public override void OnTriggerEnter(Collider other)
+        {
+            base.OnTriggerEnter(other);
+            hasGetTarget = true;
+        }
+        
+    }
+}
