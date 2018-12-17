8592ecc32ce8032ef27e061b158376827ac41b4e
Unknown <irrsfunmaker@gmail.com>
2018-12-17 12:54:04 +0900

    敵の攻撃判定を入れました

--- BubbleGame/Assets/Scripts/AirGunCollision.cs
+++ BubbleGame/Assets/Scripts/AirGunCollision.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class AirGunCollision : MonoBehaviour
 {
+    [SerializeField]
+    private float airGunPower;
     void OnParticleCollision(GameObject _obj)
     {
         Debug.Log(_obj);
         if (_obj.layer == 10 /*Bubble*/)
         {
             Vector3 particleDirection = this.transform.forward;
-            _obj.GetComponent<BubbleController>().AddForceByPush(particleDirection*3);
+            _obj.GetComponent<BubbleController>().AddForceByPush(particleDirection* airGunPower);
         }
     }
 }
--- BubbleGame/Assets/Scripts/SnapAttackEffCtr.cs
+++ BubbleGame/Assets/Scripts/SnapAttackEffCtr.cs
@@ -5,50 +5,11 @@ using UnityEngine;
 
 public class SnapAttackEffCtr : MonoBehaviour
 {
-    private ParticleSystem[] particleSystems;
-
-    [SerializeField]
-    private float waveSpeed = 1f;
-
-    [SerializeField]
-    private BehaviorTree waveAttack;
-
-    private float initStartSize;
-
-    private float startSize;
-    private bool canStartWaveAttack = true;
-    // Use this for initialization
-    void Start()
+    private void OnParticleCollision(GameObject _other)
     {
-        particleSystems = GetComponentsInChildren<ParticleSystem>();
-        var ma = particleSystems[0].main;
-        initStartSize = ma.startSize.constant;
-        startSize = initStartSize;
-        canStartWaveAttack = (bool)waveAttack.GetVariable("CanStartWaveAttack").GetValue();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        canStartWaveAttack = (bool)waveAttack.GetVariable("CanStartWaveAttack").GetValue();
-        if (canStartWaveAttack)
+        if (_other.CompareTag("Player"))
         {
-            foreach (var particle in particleSystems)
-            {
-                startSize += Time.fixedDeltaTime * 60 * waveSpeed;
-                var ma = particle.main;
-                ma.startSize = new ParticleSystem.MinMaxCurve(startSize, startSize);
-
-            }
-        }
-        else
-        {
-            foreach (var particle in particleSystems)
-            {
-                var ma = particle.main;
-                ma.startSize = new ParticleSystem.MinMaxCurve(initStartSize, initStartSize);
-                startSize = initStartSize;
-            }
+            _other.GetComponent<PlayerController>().Damage();
         }
     }
 }
