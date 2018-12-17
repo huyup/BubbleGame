2674a4c5e073d04c76619e28af6c79a806a490f4
Unknown <irrsfunmaker@gmail.com>
2018-12-17 11:19:55 +0900

    AI修正

---
+++ BubbleGame/Assets/Scripts/NeedEffAttack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedEffAttack : MonoBehaviour {
+
+    private void OnParticleCollision(GameObject _other)
+    {
+        if (_other.CompareTag("Player"))
+        {
+            _other.GetComponent<PlayerController>().Damage();
+        }
+    }
+}
